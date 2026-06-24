48f48fae078f31539cdb54f08710b3522115f4d9
yxz <w15239627556@163.com>
2026-06-24 14:10:20 +0800

    文件名称

--- backend/services/task_service.py
+++ backend/services/task_service.py
@@ -329,7 +329,7 @@ class TaskService:
 
     def upload_image(self, task_id, row_index, col_index, tile_image):
         """任务模式：上传拼图块到指定任务，DPI/smear_type 从 task_info 取"""
-        filename = f"{task_id}_{row_index}_{col_index}.jpg"
+        filename = tile_image.filename
         # 记录日志，task_id,file_name,接收到图片的时间,转换为时分秒毫秒
         logger.info("task_id=%s, file_name=%s, 接收到图片的时间：%s", task_id, filename, datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
         image_bytes = tile_image.read()
