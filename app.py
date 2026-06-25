9ce6db64f5ee0a5d036fecc2e8cc216dfe1c623e
yxz <w15239627556@163.com>
2026-06-25 11:39:53 +0800

    导包

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 backend_dir = os.path.join(root_dir, 'backend')
 algorithms_dir = os.path.join(root_dir, 'algorithms')
 project_dir = os.path.join(root_dir, 'project')
+triton_dir = os.path.join(root_dir, 'triton')
 sys.path.append(root_dir)
 sys.path.append(backend_dir)
 sys.path.append(algorithms_dir)
 sys.path.append(project_dir)
+sys.path.append(triton_dir)
 
 from flask import Flask, request
 from flask_cors import CORS
